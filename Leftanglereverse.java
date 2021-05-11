793cf047c9001928ff3edeac8a570e82f0895a8f
Sunilg66 <sunilkg251@gmail.com>
2021-05-11 20:07:17 +0530

    adding file

---
+++ Leftanglereverse.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Leftanglereverse{
+
+public static void main(String []a ){
+
+Scanner sc= new Scanner(System.in);
+
+System.out.println("enter the number of row");
+
+int row= sc.nextInt();
+
+for (int i=1; i<=row; i++){
+
+for (int j=1; j<=row; j++){
+
+if(i<=j)
+
+System.out.print("* ");
+
+}
+
+System.out.println();
+}
+}
+}
