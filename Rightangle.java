93937cdecbd604f9cc97b41744f2fe5167a04585
Sunilg66 <sunilkg251@gmail.com>
2021-05-11 17:32:50 +0530

    adding file

---
+++ Rightangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Rightangle{
+
+public static void main(String []a ){
+
+Scanner sc= new Scanner(System.in);
+
+System.out.println("enter the number of row");
+
+int row= sc.nextInt();
+
+for (int i=1; i<=row; i++){
+
+for (int j=1; j<=row; j++){
+
+if(i>=j)
+
+System.out.print("* ");
+
+else
+
+System.out.print("   ");
+
+}
+
+System.out.println();
+}
+}
+}
