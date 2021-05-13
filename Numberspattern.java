6f18034ae918eaefc2ddad4fdcb9f5b2ed37860e
Sunilg66 <sunilkg251@gmail.com>
2021-05-13 20:22:29 +0530

    adding file

---
+++ Numberspattern.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Numberspattern{
+
+public static void main(String []a ){
+
+Scanner sc= new Scanner(System.in);
+
+System.out.println("enter the number of row");
+
+byte row= sc.nextByte();
+
+for (int i=1; i<=row; i++){
+
+for (int j=1; j<=row; j++){
+
+if(i+j>row)
+
+System.out.print(" "+j);
+
+else
+
+System.out.print("  ");
+}
+System.out.println();
+}
+}
+}
